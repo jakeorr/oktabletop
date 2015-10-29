f9b42910478469d318a6285aa71f39b74baff128
jakeorr <jake.a.orr@gmail.com>
2015-10-29 13:07:55 -0700

    Moving bower components to public/.

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ var app = express();
 app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(__dirname + '/public'));
-app.use('/vendor', express.static(path.join(__dirname, 'bower_components')));
 
 // views is directory for all template files
 app.set('views', __dirname);
